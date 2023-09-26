d9a06745ba01c8298a4d31c9a8121a263f8c98ac
Thomas Pfaff <thomas.pfaff@istec.de>
2023-09-26 12:35:15 +0200

    change tsup config

---
+++ tsup.config.ts
@@ -0,0 +1,11 @@
+import type { Options } from 'tsup'
+
+const config: Options = {
+  entry: ['src/index.ts'],
+  dts: true,
+  sourcemap: true,
+  format: ['cjs', 'esm'],
+  clean: true,
+}
+
+export default config
